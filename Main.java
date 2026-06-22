81d5cd43dac6dd0b7c4e139b620a2f077a9633b7
PichDesuwa <pichbothun@gmail.com>
2026-06-22 16:21:56 +0700

    adada

--- Main.java
+++ Main.java
@@ -1,3 +1,20 @@
-public class Main {
-    
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import scene_main.Controller;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage Stage) throws Exception{
+        var view_main = getClass().getResource("scene_main/View.fxml");
+        var loader = new FXMLLoader();
+        loader.setLocation(view_main);
+
+        var scene = new Scene(loader.load());
+
+        Stage.setScene(scene);
+        Stage.show();
+    }
 }
