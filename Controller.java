ef41f5c40d4c0d1586280ac8740a441cb3d2d7e8
mAiyc <lyzatouy@gmail.com>
2026-06-22 15:57:59 +0700

    wedrf

---
+++ Controller.java
@@ -0,0 +1,36 @@
+package scene_main;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+public class Controller {
+
+    @FXML
+    private Label label_status;
+
+    @FXML
+    private ListView<?> list_view;
+
+    @FXML
+    void on_create(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_delete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_read(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_update(ActionEvent event) {
+
+    }
+
+}
