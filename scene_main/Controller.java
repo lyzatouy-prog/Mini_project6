f6814b07f147db870f010e31c592f1cbb1c0265f
mAiyc <lyzatouy@gmail.com>
2026-06-22 16:54:03 +0700

    wertyu

--- scene_main/Controller.java
+++ scene_main/Controller.java
@@ -1,5 +1,9 @@
 package scene_main;
 
+import java.nio.file.Files;
+import java.sql.Array;
+import java.util.ArrayList;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -25,15 +29,39 @@ public class Controller {
     }
 
     @FXML
-    void on_read(ActionEvent event) {
+    void on_read(ActionEvent event) throws Exception {
+
         // object that help to choose file
+
         var stage = (Stage) label_status.getScene().getWindow();
         var  file_chooser = new javafx.stage.FileChooser();
         var file = file_chooser.showOpenDialog(stage);
+
+
+        var data = new ArrayList<String>();
+        for (var line : Files.readAllLines(file.toPath())) {
+            data.add((String) line);
+        }
+
+        list_view.getItems().addAll(data);
+
     }
 
     @FXML
     void on_update(ActionEvent event) {
+
+    }
+
+    @FXML
+    void intialize() {
+
+        var data = new ArrayList<String>();
+        data.add("Orange");
+        data.add("Apple");
+        data.add("Banana");
+
+        // add data to list view
+        list_view.getItems().addAll(data);
 
     }
 
