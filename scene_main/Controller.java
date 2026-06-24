de752204bd3ec5b9ece5dc997dbde0b3259b0aeb
PichDesuwa <pichbothun@gmail.com>
2026-06-24 14:19:17 +0700

    fix

--- scene_main/Controller.java
+++ scene_main/Controller.java
@@ -40,8 +40,21 @@ public class Controller {
     }
 
     @FXML
-    void on_delete(ActionEvent event) {
+    void on_delete(ActionEvent event) throws Exception {
+        var node = (Node) event.getSource();
+        var stage = (Stage) label_status.getScene().getWindow();
+        var view_create = getClass().getResource("../scene_delete/View.fxml");
+        
+        var controller_create = new scene_delete.Controller();
+        var loader = new FXMLLoader();
+        loader.setController(controller_create);
+        loader.setLocation(view_create);
 
+        var scene = new Scene(loader.load());
+
+        stage.setScene(scene);
+        stage.show();
+        
     }
 
     @FXML
@@ -64,7 +77,7 @@ public class Controller {
     }
 
     @FXML
-    void on_update(ActionEvent event) {
+    void on_update(ActionEvent event) throws Exception {
 
     }
 
