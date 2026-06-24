aedccd1c163b6f892cefc322280dbf95326a9ea5
saydara2004-source <say.dara2004@gmail.com>
2026-06-24 14:23:50 +0700

    iuytgfds

--- scene_create/Controller.java
+++ scene_create/Controller.java
@@ -3,6 +3,7 @@ package scene_create;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -18,17 +19,18 @@ public class Controller {
 
     @FXML
     void on_save(ActionEvent event) throws Exception{
- var node = (Node) event.getSource();
+ 
+        var node = (Node) event.getSource();
         var stage = (Stage) node.getScene().getWindow();
-        var view_create = getClass().getResource("../scene_create/View.fxml");
-        
-        var controller_create = new scene_create.Controller();
-        var loader = new FXMLLoader();
-        loader.setController(controller_create);
-        loader.setLocation(view_create);
+
+        var view_main = getClass().getResource("/scene_main/View.fxml");
+        var controller_main = new scene_main.Controller();
+
+        var loader = new javafx.fxml.FXMLLoader();
+        loader.setLocation(view_main);
+        loader.setController(controller_main);
 
         var scene = new Scene(loader.load());
-
         stage.setScene(scene);
         stage.show();
     }
