ef5d9bcca5ee452dc7ab281318a3b24d4fd98a77
SOPHANITH <yoongsophanith18@gmail.com>
2026-06-24 13:30:23 +0700

    ss

---
+++ scene_delete/Controller.java
@@ -0,0 +1,15 @@
+import javafx.event.ActionEvent;
+
+public class Controller {
+
+    @FXML
+    void on_no(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_yes(ActionEvent event) {
+
+    }
+
+}
