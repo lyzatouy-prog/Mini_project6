5692b523f06d29cf72acb27218e3efd57df0c93d
PichDesuwa <pichbothun@gmail.com>
2026-06-24 14:15:24 +0700

    adad

--- scene_main/Controller.java
+++ scene_main/Controller.java
@@ -6,6 +6,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
