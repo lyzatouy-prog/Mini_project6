d61739c1f02f6c64270ac4d63dab7f4233d5535f
SOPHANITH <yoongsophanith18@gmail.com>
2026-06-24 13:57:37 +0700

    dd

--- scene_delete/Controller.java
+++ scene_delete/Controller.java
@@ -3,6 +3,9 @@ package scene_delete;
 
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 
 public class Controller {
@@ -13,9 +16,21 @@ public class Controller {
     }
 
     @FXML
-    void on_yes(ActionEvent event) {
+    void on_yes(ActionEvent event) throws Exception {
 
-        
+      var node = (Node) event.getSource();
+      var stage = (Stage) node.getScene().getWindow();
+
+      var view_main = getClass().getResource("/scene_main/View.fxml");
+      var controller_main = new scene_main.Controller();
+
+      var loader = new javafx.fxml.FXMLLoader();
+      loader.setLocation(view_main);
+      loader.setController(controller_main);
+
+      var scene = new Scene(loader.load());
+      stage.setScene(scene);
+      stage.show();
 
     }
 
